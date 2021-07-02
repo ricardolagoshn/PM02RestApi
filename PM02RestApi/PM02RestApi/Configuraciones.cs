00f10e64eaa57e43d22e28a32581c35eae09bb96
Ricardo.lagos <ricardolagoshn>
2021-07-01 20:14:11 -0600

    Add project files.

---
+++ PM02RestApi/PM02RestApi/Configuraciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM02RestApi
+{
+    public class Configuraciones
+    {
+        public const String IDFoursquare = "SJYNWEB44YOBAAY0VIZTPKE43VNXJWTXL3NH5MNAG4FZAZPV";
+        public const String SecreteFoursquare = "I3KD5CINMV4YS2FUYQCLNWGJV0MSYQDU5Y5EGWI4NVQSLNBL";
+
+        public const String apifoursquare = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+    }
+
+
+    public class Sitios
+    {
+        public static String getUrl(Double latitud, Double longitud)
+        {
+            return String.Format(Configuraciones.apifoursquare, latitud, longitud, Configuraciones.IDFoursquare, Configuraciones.SecreteFoursquare, DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
---
+++ PM02RestApi/PM02RestApi/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static PM02RestApi.Countries;
+using PM02RestApi;
+
+namespace PM02RestApi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRestApi_Clicked(object sender, EventArgs e)
+        {
+
+            var coordenadas = Plugin.Geolocator.CrossGeolocator.Current;
+            var posicion = await coordenadas.GetPositionAsync();
+
+            list.ItemsSource =  await AppClass.Metodos.getSites(posicion.Latitude, posicion.Longitude);
+
+            #region test
+                    /*
+                  var url = "https://restcountries.eu/rest/v2/region/europe";
+
+                  var urlfq = Sitios.getUrl(14.66, -88.05);
+
+                  using (HttpClient cliente = new HttpClient())
+                  {
+                      var Respuesta = await cliente.GetAsync(url);
+
+                      if (Respuesta.IsSuccessStatusCode)
+                      {
+                          var json =  Respuesta.Content.ReadAsStringAsync().Result;
+
+                          await DisplayAlert("Respuesta", json, "OK");
+
+                          var Paises = JsonConvert.DeserializeObject<List<CountriesREst>>(json);
+
+
+
+                      }
+
+                  }
+                  */
+            #endregion
+
+        }
+    }
+}
